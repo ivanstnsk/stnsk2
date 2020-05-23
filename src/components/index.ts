99b54ef25f6fecd3931cc3163c49c8ab939d0c47
Ivan Stinsky <ivan.stnsk@gmail.com>
2020-05-23 17:02:52 +0300

    [all] Add components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { IntroCircle } from './IntroCircle';
 export { GradientBackground } from './GradientBackground';
 export { MainButton } from './MainButton';
 export { Footer } from './Footer';
+export { Fallback } from './Fallback';
+export { ContentHeader } from './ContentHeader';
