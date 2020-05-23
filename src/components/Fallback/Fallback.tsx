dcbfd40b2f2a70549292f289a5d4a3d8f69622f4
Ivan Stinsky <ivan.stnsk@gmail.com>
2020-05-23 17:02:40 +0300

    [all] Add Fallback component

---
+++ src/components/Fallback/Fallback.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {useStyles} from './styles';
+
+
+export const Fallback: React.FC<{}> = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container} />
+  );
+};
---
+++ src/components/Fallback/styles.ts
@@ -0,0 +1,17 @@
+import {AppStyles} from 'types/style';
+import {useTStyles} from 'hooks';
+
+const styles: AppStyles = ({colors: {primary: {bg}}}) => {
+  return {
+    container: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: bg,
+      overflow: 'hidden',
+    },
+  };
+};
+
+type TStyles = Record<string, any>;
+
+export const useStyles = (): TStyles => useTStyles(styles);
