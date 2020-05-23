17006a9e7bb9a3801d07b78848a0b4e9013c57b0
Ivan Stinsky <ivan.stnsk@gmail.com>
2020-05-23 17:02:19 +0300

    [all] Add ContentHeader component

---
+++ src/components/ContentHeader/ContentHeader.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {useStyles} from './styles';
+
+
+interface ContentHeaderProps {
+  title: string;
+  subTitle: string;
+}
+
+export const ContentHeader: React.FC<ContentHeaderProps> = ({
+  title,
+  subTitle,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.title}>
+        {title}
+      </div>
+      <div className={classes.subTitle}>
+        {subTitle}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/ContentHeader/styles.ts
@@ -0,0 +1,30 @@
+import {AppStyles} from 'types/style';
+import {useTStyles} from 'hooks';
+
+const styles: AppStyles = ({colors: {typography: {title, subTitle}}}) => {
+  return {
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+    },
+    title: {
+      fontSize: 50,
+      fontWeight: 300,
+      lineHeight: '62px',
+      color: title,
+    },
+    subTitle: {
+      fontSize: 18,
+      fontWeight: 300,
+      lineHeight: '22px',
+      color: subTitle,
+      marginTop: 5,
+    },
+  };
+};
+
+type TStyles = Record<string, any>;
+
+export const useStyles = (): TStyles => useTStyles(styles);
