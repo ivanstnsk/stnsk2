e43fc9e8ea5d6d068037e9221d0df1bf49376029
Ivan Stinsky <ivan.stnsk@gmail.com>
2020-05-25 22:55:22 +0300

    [laptop] Fix About screen

--- src/screens/About/laptop/styles.ts
+++ src/screens/About/laptop/styles.ts
@@ -8,9 +8,6 @@ const styles: AppStyles = ({
       bgContent, greyUltralight,
     },
   },
-  sizes: {
-    content: { offsetHorizontal, offsetTop, offsetBottom },
-  },
 }) => {
   return {
     container: {
@@ -24,17 +21,6 @@ const styles: AppStyles = ({
       alignItems: 'stretch',
       backgroundColor: bgContent,
       overflow: 'hidden',
-    },
-    content: {
-      display: 'flex',
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'flex-start',
-      alignItems: 'stretch',
-      paddingTop: offsetTop,
-      paddingBottom: offsetBottom,
-      paddingLeft: offsetHorizontal,
-      paddignRight: offsetHorizontal,
     },
     colsContainer: {
       display: 'flex',
