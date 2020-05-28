6b5349456beaa9c4f8104438ba2db37c9d2df004
ivanstnsk <ivan.stnsk@gmail.com>
2020-05-29 01:09:01 +0300

    [laptop] Update ContentContainer

--- src/components/ContentContainer/ContentContainer.tsx
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
+import { combineClasses } from 'utils/styles';
+
 import { useStyles } from './styles';
 
 
 interface ContentContainerProps {
   children: JSX.Element | JSX.Element[];
+  className?: string;
 }
 
 export const ContentContainer: React.FC<ContentContainerProps> = ({
   children,
+  className,
 }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.wrapper}>
+    <div className={combineClasses(classes.wrapper, className)}>
       <div className={classes.container}>
         {children}
       </div>
