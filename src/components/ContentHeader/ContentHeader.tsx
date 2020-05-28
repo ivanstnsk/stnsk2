0a4d31b6f679633625eacab7f0c36198da124660
ivanstnsk <ivan.stnsk@gmail.com>
2020-05-29 01:09:16 +0300

    [laptop] Update ContentHeader

--- src/components/ContentHeader/ContentHeader.tsx
+++ src/components/ContentHeader/ContentHeader.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import { TContentSize } from 'types/sizes';
+
 import { useStyles } from './styles';
 
 
 interface ContentHeaderProps {
   title: string;
   subTitle: string;
+  size?: TContentSize;
 }
 
 export const ContentHeader: React.FC<ContentHeaderProps> = ({
   title,
   subTitle,
+  size = 'normal',
 }) => {
-  const classes = useStyles();
+  const classes = useStyles({ size });
 
   return (
     <div className={classes.container}>
