055b96313d930e233542a64d8765d776e8e29a6b
ivanstnsk <ivan.stnsk@gmail.com>
2020-05-29 19:34:53 +0300

    [project] Fix build version generator

--- scripts/generate-build-id.js
+++ scripts/generate-build-id.js
@@ -4,7 +4,7 @@ fs.readFile('src/metadata.json', (err, content) => {
   if (err) throw err;
   const metadata = JSON.parse(content);
   const lastBuildNumber = `${metadata.buildVersion.major}.${metadata.buildVersion.minor}.${metadata.buildVersion.patch}`;
-  metadata.buildVersion.patch += 1;
+  metadata.buildVersion.patch = process.env.TRAVIS_BUILD_NUMBER || 'UNKNOWN_BUILD';
   fs.writeFile('src/metadata.json', JSON.stringify(metadata), (err) => {
     if (err) throw err;
 
